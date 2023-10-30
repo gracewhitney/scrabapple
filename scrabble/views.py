3a584c718a83c79f81937e8f71ee3e018887e7ea
Grace Whitney <grace@zagaran.com>
2023-10-30 17:11:19 -0400

    Make migrations

--- scrabble/views.py
+++ scrabble/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import FormView
+
+from scrabble.forms import CreateGameForm
+from scrabble.models import ScrabbleGame
+
 
 # Create your views here.
+class CreateGameView(FormView):
+    template_name = "scrabble/new_game.html"
+    form_class = CreateGameForm
+
+    def form_valid(self, form):
+        game = ScrabbleGame.objects.create()
+
+
