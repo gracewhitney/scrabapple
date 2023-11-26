7a5ae8ff700455b3e4c74c7fc25fa65b1ed459d7
Grace Whitney <grace@zagaran.com>
2023-11-26 14:39:48 -0500

    Scoring

--- scrabble/serializers.py
+++ scrabble/serializers.py
@@ -1,13 +1,20 @@
+from django.core.exceptions import ValidationError
 from rest_framework import serializers
 
 from scrabble.constants import TurnAction
 
 
 class TileMoveSerializer(serializers.Serializer):
-    tile = serializers.CharField(max_length=1)
-    blank_letter = serializers.CharField(max_length=1, required=False)  # Replacement letter for blanks
+    tile = serializers.CharField(max_length=2)  # Blank tiles played represented as e.g. '-A'
     x = serializers.IntegerField(min_value=0, max_value=14)
     y = serializers.IntegerField(min_value=0, max_value=14)
+
+    def validate_tile(self, tile):
+        if len(tile) > 1 and tile[0] != '-':
+            raise ValidationError("Invalid tile: multiple letters")
+        if not tile[-1].isalpha():
+            raise ValidationError("Invalid tile: not letter")
+        return tile.upper()
 
 
 class GameTurnSerializer(serializers.Serializer):
--- scrabble/views.py
+++ scrabble/views.py
@@ -7,13 +7,14 @@ from django.db import transaction
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, redirect
 from django.utils.crypto import get_random_string
+from django.views import View
 from django.views.generic import FormView, TemplateView
 
 from common.models import User
 from scrabble.constants import TILE_SCORES, TurnAction
 from scrabble.forms import CreateGameForm
 from scrabble.helpers import send_invitation_email
-from scrabble.gameplay import validate_turn, calculate_points, do_turn
+from scrabble.gameplay import validate_turn, calculate_points, do_turn, create_new_game
 from scrabble.models import ScrabbleGame, GamePlayer, GameTurn
 
 
@@ -23,24 +24,9 @@ class CreateGameView(LoginRequiredMixin, FormView):
     form_class = CreateGameForm
 
     def form_valid(self, form):
-        game = ScrabbleGame.objects.create()
-        GamePlayer.objects.create(user=self.request.user, game=game, turn_index=0)
-        turn_index = 1
-        for email in [
-            form.cleaned_data["player_2_email"],
-            form.cleaned_data.get("player_3_email"),
-            form.cleaned_data.get("player_4_email")
-        ]:
-            if not email:
-                break
-            user, created = User.objects.get_or_create(
-                email=email, defaults={"one_time_passcode": get_random_string(32)}
-            )
-            GamePlayer.objects.create(user=user, game=game, turn_index=turn_index)
-            send_invitation_email(user, game.id, new_user=created)
-            turn_index += 1
+        game = create_new_game(form, self.request.user)
         messages.success(
-            self.request, f"New game created with {turn_index + 1} players. Invitation emails have been issued."
+            self.request, f"New game created with {game.racks.count()} players. Invitation emails have been issued."
         )
         return redirect("play_scrabble", game_id=game.id)
 
@@ -66,16 +52,14 @@ class ScrabbleView(GamePermissionMixin, TemplateView):
         return context
 
 
-class ScrabbleTurnView(GamePermissionMixin, TemplateView):
+class ScrabbleTurnView(GamePermissionMixin, View):
     def post(self, request, *args, **kwargs):
         turn_data = json.loads(request.body.decode())
         game_player = GamePlayer.objects.get(game=self.scrabble_game, user=self.request.user)
-        try:
-            cleaned_turn_data = validate_turn(turn_data, self.scrabble_game, game_player)
-        except ValidationError as e:
-            return JsonResponse(status=400, data={"error": e.message})
-        turn_score, updated_rack, updated_letter_bag = do_turn(cleaned_turn_data, self.scrabble_game, game_player)
         with transaction.atomic():
-            GameTurn.objects.create(
-                game_player
-            )
+            try:
+                cleaned_turn_data = validate_turn(turn_data, self.scrabble_game, game_player)
+            except ValidationError as e:
+                return JsonResponse(status=400, data={"error": e.message})
+            do_turn(cleaned_turn_data, self.scrabble_game, game_player)
+        return redirect('scrabble:play_scrabble', game_id=self.scrabble_game.id)
