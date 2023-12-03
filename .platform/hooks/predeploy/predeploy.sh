5a7d3a6c02ee466c0a10bcab6d5bbf46921f7d28
Grace Whitney <grace@zagaran.com>
2023-12-03 12:18:02 -0500

    Deployment fixes

--- .platform/hooks/predeploy/predeploy.sh
+++ .platform/hooks/predeploy/predeploy.sh
@@ -1,23 +1,14 @@
 #!/bin/bash
 set -e
 
-# START_FEATURE elastic_beanstalk
 source $PYTHONPATH/activate
 
-# START_FEATURE django_react, sass_bootstrap
-npm install --production
-# END_FEATURE django_react, sass_bootstrap
+NODE_OPTIONS=--max_old_space_size=400 npm install --omit:dev
 
-# START_FEATURE django_react
-# delete old webpack static resources
 rm -rf static/webpack_bundles/ || echo "no webpack bundles to remove"
 rm -rf staticfiles/webpack_bundles/ || echo "no staticfiles webpack bundles to remove"
-$(npm bin)/nwb build --no-vendor
-# END_FEATURE django_react
+NODE_OPTIONS=--openssl-legacy-provider npm run build
 
-# START_FEATURE sass_bootstrap
 python manage.py compilescss
-# END_FEATURE sass_bootstrap
 
 python manage.py collectstatic --noinput --ignore *.scss
-# END_FEATURE elastic_beanstalk
