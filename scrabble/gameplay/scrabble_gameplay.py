a7eb13ac125d5ab9459981ac2c6dcd3e00cd697f
Grace Whitney <grace@zagaran.com>
2025-01-11 17:16:17 -0500

    Undo scrabble changes

--- scrabble/gameplay/scrabble_gameplay.py
+++ scrabble/gameplay/scrabble_gameplay.py
@@ -63,15 +63,14 @@ class ScrabbleCalculator(BaseGameCalculator):
         tile_index = 0
         word_multiplier = 1
         points = 0
-        blank_indexes = {}
         for x in range(start_x, self.board_size):
             tile = board.get_tile(x, row)
             if tile:
                 points += TILE_SCORES[tile[0]]
                 word += tile[-1]
             elif tile_index < len(played_tiles) and played_tiles[tile_index]['x'] == x:
-                tile = played_tiles[tile_index]['tile'][0]
-                letter_points = TILE_SCORES[tile]
+                tile = played_tiles[tile_index]['tile']
+                letter_points = TILE_SCORES[tile[0]]
                 multiplier = BOARD_CONFIG[x][row]
                 if multiplier == Multiplier.dl:
                     letter_points *= 2
@@ -82,16 +81,13 @@ class ScrabbleCalculator(BaseGameCalculator):
                 if multiplier == Multiplier.tw:
                     word_multiplier *= 3
                 points += letter_points
-                # Keep track of word index -> blank tile mapping for validation
-                if tile == "-":
-                    blank_indexes[len(word)] = played_tiles[tile_index]
-                word += tile
+                word += tile[-1]
                 tile_index += 1
             else:
                 break
         points *= word_multiplier
         if len(word) > 1:
-            return points, word,
+            return points, word
         return 0, None
 
     def get_unplayed_tile_points(self, tile):
