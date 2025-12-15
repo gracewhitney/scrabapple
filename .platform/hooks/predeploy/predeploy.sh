fdefef4b0e9446100b899fc673fa9613c2e70681
Grace Whitney <grace@zagaran.com>
2025-12-15 18:49:28 -0500

    Swap deletion order

--- .platform/hooks/predeploy/predeploy.sh
+++ .platform/hooks/predeploy/predeploy.sh
@@ -3,10 +3,9 @@ set -e
 
 source $PYTHONPATH/activate
 
-NODE_OPTIONS=--max_old_space_size=400 npm install --omit:dev
-
 rm -rf static/webpack_bundles/ || echo "no webpack bundles to remove"
 rm -rf staticfiles/ || echo "no staticfiles to remove"
+NODE_OPTIONS=--max_old_space_size=400 npm install --omit:dev
 NODE_OPTIONS=--openssl-legacy-provider npm run build
 
 python manage.py compilescss
