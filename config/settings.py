01c214cf44ed4cc6c4bf5276fda64eb9c9f494c6
Grace Whitney <grace@zagaran.com>
2026-01-13 12:51:48 -0500

    Try removing node_modules from staticfiles

--- config/settings.py
+++ config/settings.py
@@ -229,7 +229,7 @@ STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-    os.path.join(BASE_DIR, "node_modules"),
+    #os.path.join(BASE_DIR, "node_modules"),
 ]
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
